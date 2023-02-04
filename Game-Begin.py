bc8cb8f600cb5ef4df0a5464570bb3a258bd2d70
Erez Sarousi <66281623+ESarousi@users.noreply.github.com>
2023-02-04 14:18:15 -0500

    Update Game-Begin.py

--- Game-Begin.py
+++ Game-Begin.py
@@ -1,3 +1,56 @@
+P1 = {
+    'ID': 1,
+    'Name': 'John Smith',
+    'Age': 62,
+    'Sex': 'Male',
+    'Allergies': ['Oxycodone'],
+    'Reactions': 'Hives',
+    'Chief Complaint': 'Vomiting',
+    'Complaints': ['Abdominal Pain', 'Lethargy', 'Jaundice', 'Lack of Appetite', 'Dark Urine'],
+    'Diagnosis': 'Liver Cancer',
+    'Weight': 244,
+    'Height': 65,
+    'Blood Glucose': 141,
+    'Blood Pressure': 136,
+    'Temperature': 99.4
+     }
+
+P2 = {
+    'ID': 2,
+    'Name': 'Sally Samson',
+    'Age': 49,
+    'Sex': 'Female',
+    'Allergies': [None],
+    'Reactions': None,
+    'Chief Complaint': 'Trouble Breathing',
+    'Complaints': ['Sore Throat', 'Congestion', 'Fatigue', 'Headache', 'Lost of Taste'],
+    'Diagnosis': 'COVID-19',
+    'Weight': 157,
+    'Height': 61,
+    'Blood Glucose': 105,
+    'Blood Pressure': 113,
+    'Temperature': 101.7
+     }
+
+P3 = {
+    'ID': 3,
+    'Name': 'Michael Jordanson',
+    'Age': 36,
+    'Sex': 'Male',
+    'Allergies': 'Penicillin',
+    'Reactions': 'Anaphylaxis',
+    'Chief Complaint': 'Cramping in Leg',
+    'Complaints': ['', 'Lethargy', 'Dry Mouth', 'Nausea'],
+    'Diagnosis': 'Diabetes Mellitus Type 2',
+    'Weight': 257,
+    'Height': 68,
+    'Blood Glucose': 225,
+    'Blood Pressure': 150,
+    'Temperature': 98.4
+     }
+
+Patient_List = [P1, P2, P3]
+
 def game_begin():
     patient = random.choice(Patient_List)
     def menu():
